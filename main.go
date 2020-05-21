2cc2d5590358a77b3050f31cda8aca94a1c4e084
Ben Jackson <b@benjackson.email>
2020-05-21 21:08:20 +1000

    leader election id

--- main.go
+++ main.go
@@ -57,6 +57,7 @@ func main() {
 		Scheme:             scheme,
 		MetricsBindAddress: metricsAddr,
 		LeaderElection:     enableLeaderElection,
+		LeaderElectionID:   "principio-leader-election-helper",
 		Port:               9443,
 	})
 	if err != nil {
